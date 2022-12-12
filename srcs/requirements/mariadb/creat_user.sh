3a951d498f7840982b177a4dec6de843e8db8d2d
zsarir23 <zainebsarir@gmail.com>
2022-12-12 02:12:51 +0100

    wordpress

--- srcs/requirements/mariadb/creat_user.sh
+++ srcs/requirements/mariadb/creat_user.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-# sed -i 's/#port                   = 3306/port = 3306/' 50-server.cnf
-# sed -i 's/bind-address            = 127.0.0.1/bind-address            = 0.0.0.0/' 50-server.cnf
+sed -i 's/#port                   = 3306/port = 3306/' 50-server.cnf
+sed -i 's/bind-address            = 127.0.0.1/bind-address            = 0.0.0.0/' 50-server.cnf
 
 echo "CREATE DATABASE myDB;" | mysql -u root
 
-echo "GRANT ALL ON *.* TO '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;" | mysql -u root
+echo "USE myDB;" | mysql -u root
 
-echo "GRANT ALL PRIVILEGES ON myDB.* TO '$MYSQL_USER'@localhost IDENTIFIED BY '$MYSQL_PASSWORD';"  | mysql -u root
+echo "GRANT ALL ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;" | mysql -u root
+
+echo "GRANT ALL PRIVILEGES ON myDB.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"  | mysql -u root
 
 echo "FLUSH PRIVILEGES;" | mysql -u root
 
--- srcs/requirements/wordpress/wordpress_config.sh
+++ srcs/requirements/wordpress/wordpress_config.sh
@@ -8,13 +8,15 @@ mv wp-cli.phar /usr/local/bin/wp
 
 sed -i -e 's/listen =.*/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
-wp core download --allow-root
+wp --allow-root core download
 
-# wp core config --dbname=myDB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --dbprefix=wp_ --skip-check --allow-root
+wp core config --dbname=myDB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --dbprefix=wp_ --allow-root --skip-check
 
-# wp core install --url="localhost" --title="My Site" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" --admin_email="zsarir@email.com" --allow-root --path=/var/www/html
+wp --allow-root  core install --url="localhost" --title="My Site" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" --admin_email="zsarir@email.com"
 
 # Utilisation de WP-CLI pour installer et gérer les thèmes WordPress
 # wp theme activate twentyfifteen --allow-root
-# wp theme install ultrabootstrap --activate --allow-root
+wp theme install ultrabootstrap --activate --allow-root
 
+mkdir -p /run/php/
+php-fpm7.3 -F
