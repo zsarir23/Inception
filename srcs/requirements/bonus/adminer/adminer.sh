46899297c3a121e3b8ad2f15e40aa194e13fda1b
zsarir23 <zainebsarir@gmail.com>
2023-01-11 21:05:20 +0100

    adminer

--- srcs/requirements/bonus/adminer/adminer.sh
+++ srcs/requirements/bonus/adminer/adminer.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-mkdir /var/www/html/Adminer
+cd /var/www/html/
 
-cd /var/www/html/Adminer
+wget https://www.adminer.org/latest.php -O adminer.php 
 
-wget https://github.com/vrana/adminer/releases/download/v4.7.3/adminer-4.7.3.php && mv adminer-4.7.3.php adminer.php 
+chown -R www-data:www-data /var/www/html/adminer.php
 
-chown -R www-data:www-data /var/www/html/Adminer/adminer.php 
-
-chmod 755 /var/www/html/Adminer/adminer.php
+chmod 755 /var/www/html/adminer.php
 
 php -S 0.0.0.0:8080
